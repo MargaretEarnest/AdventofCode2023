676545b97504020517d321bef2b7013cd35af752
Margaret Earnest <molly7312000@gmail.com>
2023-12-01 23:56:12 -0500

    Switched findAll to find

--- src/day_1/day_1.kt
+++ src/day_1/day_1.kt
@@ -2,15 +2,15 @@ package day_1
 
 import java.io.File
 
-const val isPartTwo = true
+const val isPartTwo = false
 val digits = mapOf("one" to "1", "two" to "2", "three" to "3", "four" to "4", "five" to "5", "six" to "6",
         "seven" to "7", "eight" to "8", "nine" to "9")
 
 fun getResult(line: String): Int {
     val target = if (isPartTwo) "one|two|three|four|five|six|seven|eight|nine" else ""
     val sep = if (isPartTwo) "|" else ""
-    var firstNum = "($target$sep\\d)".toRegex().findAll(line).first().groupValues[0]
-    var secondNum = "(${target.reversed()}$sep\\d)".toRegex().findAll(line.reversed()).first().groupValues[0].reversed()
+    var firstNum = "($target$sep\\d)".toRegex().find(line)?.groupValues?.get(0)
+    var secondNum = "(${target.reversed()}$sep\\d)".toRegex().find(line.reversed())?.groupValues?.get(0)?.reversed()
     if (firstNum in digits) firstNum = digits.getOrDefault(firstNum, "")
     if (secondNum in digits) secondNum = digits.getOrDefault(secondNum, "")
     return (firstNum + secondNum).toInt()
