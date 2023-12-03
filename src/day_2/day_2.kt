e4469ec3264f78051502d73b8e805ec31b89e5f3
MargaretEarnest <34314628+MargaretEarnest@users.noreply.github.com>
2023-12-03 17:28:50 -0500

    Day 2 improvements

--- src/day_2/day_2.kt
+++ src/day_2/day_2.kt
@@ -3,10 +3,9 @@ package day_2
 import java.io.File
 import kotlin.math.max
 
-const val isPartTwo = true
+const val isPartTwo = false
 
 fun main() {
-    val limits = mapOf("red" to 12, "blue" to 14, "green" to 13)
     var sum = 0
     File(System.getProperty("user.dir")+ "\\src\\day_2\\data").forEachLine { line ->
         val idAndData = line.split(": ")
@@ -22,15 +21,13 @@ fun main() {
             }
             minCubes.values.reduce { acc, el -> acc * el}
         } else {
-            val isImpossible = colors.map {
+            val limits = mapOf("red" to 12, "blue" to 14, "green" to 13)
+            if (colors.map {
                 it.split(", ").map { c ->
                     val d = c.split(" ")
                     limits.getOrDefault(d[1], 0) < d[0].toInt()
                 }
-            }.map { d: List<Boolean> ->
-                (true in d)
-            }.any { it }
-            if (isImpossible) 0 else id
+            }.map { true in it }.any { it }) 0 else id
         }
     }
     println(sum)
